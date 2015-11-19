24891a78b8f9a124cc0c2e1b84bf171f8983de63
Ashley Jean <amjean@loyola.edu>
2015-11-18 21:31:53 -0500

    Finished Chapter 6! Make a basic User model (including secure passwords)

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,25 +2,23 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-<<<<<<< HEAD
+
   def setup
     @user = User.new(name: "Example User", email: "user@example.com")
   end
 
-  test "should be valid" do
-    assert @user.valid?
-  end
-end
-=======
+
+
 def setup
     @user = User.new(name: "Example User", email: "user@example.com",
                      password: "foobar", password_confirmation: "foobar")
   	end
 
-  test "should be valid" do
+ test "should be valid" do
     assert @user.valid?
-	end 
- 
+  end
+
+
   test "name should be present" do
     @user.name = ""
     assert_not @user.valid?
@@ -31,15 +29,14 @@ def setup
   	assert_not @user.valid?
   	end
 
-
   test "name should not be too long" do
   	@user.name = "a" * 51
   	assert_not  @user.valid?
 	end
 
   test "email should not be too long" do
-	@user.email = "a" * 244 + "@example.com"
-	assert_not @user.valid?
+	   @user.email = "a" * 244 + "@example.com"
+	   assert_not @user.valid?
 	end
 
   test "email validation should accept valid addresses" do
@@ -51,7 +48,6 @@ def setup
     end
   end
 
-
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
@@ -60,10 +56,6 @@ def setup
       assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-<<<<<<< HEAD
- end
->>>>>>> modeling-users
-=======
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
@@ -71,5 +63,14 @@ def setup
     @user.save
     assert_not duplicate_user.valid?
   end
- end
->>>>>>> modeling-users
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+end
